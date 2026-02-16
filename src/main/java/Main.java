f284031d7e43e487025bc9f5b040abeb0abfb989
zkoukal <goldson290@gmail.com>
2026-02-16 12:59:19 +0100

    Basic operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,30 @@ public class Main {
     static void main() {
         IO.println("Projekt Zlomky");
 
-        Zlomek a  = new Zlomek(3, 4);
-        IO.println("Zlomek: " + a.getCitatel() + " / " + a.getJmenovatel());
-        IO.println(a.toString());
+        Zlomek a  = new Zlomek(1, 2);
+        Zlomek b  = new Zlomek(3, 4);
+        IO.println('+' + a.plus(b).toString());
+        IO.println('-' + a.minus(b).toString());
+        IO.println('*' + a.multiply(b).toString());
+        IO.println('/' + a.divide(b).toString());
+
+        Zlomek[] pole = new Zlomek[] {
+                new Zlomek(1, 3),
+                new Zlomek(1, 2),
+                new Zlomek(6, 5),
+                new Zlomek(4, 3),
+                new Zlomek(7, 8),
+                new Zlomek(13, 10)
+        };
+        Zlomek ap = prumer(pole);
+        IO.println("Aritmetický průměr: %s".formatted(ap.toString()));
+    }
+
+    private static Zlomek prumer(Zlomek[] pole) {
+        Zlomek temp = new Zlomek();
+        for (Zlomek z : pole) {
+            temp = temp.plus(z);
+        }
+        return temp.divide(new Zlomek(pole.length, 1));
     }
 }
--- src/main/java/zlomky/Zlomek.java
+++ src/main/java/zlomky/Zlomek.java
@@ -4,7 +4,8 @@ public class Zlomek {
     private int citatel = 0;
     private int jmenovatel = 1;
 
-    public Zlomek() {}
+    public Zlomek() {
+    }
 
     public Zlomek(int citatel, int jmenovatel) {
         if (jmenovatel == 0) {
@@ -32,9 +33,72 @@ public class Zlomek {
 
     @Override
     public String toString() {
-        return "Zlomek{" +
-                "citatel=" + citatel +
-                ", jmenovatel=" + jmenovatel +
-                '}';
+        return "Zlomek{" + citatel + " / " + jmenovatel + '}';
+    }
+
+    /**
+     * Zjednodušší zlomek na jeho základní zvar
+     *
+     * @param z Zlomek
+     * @return zlomek v základním tvaru
+     */
+    public Zlomek zkratit(Zlomek z) {
+        int nsd = nsd(z.citatel, z.jmenovatel);
+        return new Zlomek(z.citatel / nsd, z.jmenovatel / nsd);
+    }
+
+    /**
+     * Vrátí nejmenšího společného dělitele
+     *
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    private int nsd(int a, int b) {
+        int vetsi;
+        int mensi;
+        int vysledek;
+        if (a > b) {
+            vetsi = a;
+            mensi = b;
+        } else {
+            vetsi = b;
+            mensi = a;
+        }
+        if (mensi == 0) {
+            return 1;
+        }
+        do {
+            vysledek = vetsi % mensi;
+            vetsi = mensi;
+            mensi = vysledek;
+        } while (vysledek != 0);
+        return vetsi;
+    }
+
+    /**
+     * Metoda pro sčítání dvou zlomků
+     *
+     * @param a Zlomek
+     * @return nový zlomek, který je součtem obou zlomků
+     */
+    public Zlomek plus(Zlomek a) {
+        int tempCitatel = a.citatel * jmenovatel + citatel * a.jmenovatel;
+        int tempJmenovatel = a.jmenovatel * jmenovatel;
+        return zkratit(new Zlomek(tempCitatel, tempJmenovatel));
+    }
+
+    public Zlomek minus(Zlomek a) {
+        int tempCitatel = citatel * a.jmenovatel - a.citatel * jmenovatel;
+        int tempJmenovatel = a.jmenovatel * jmenovatel;
+        return zkratit(new Zlomek(tempCitatel, tempJmenovatel));
+    }
+
+    public Zlomek multiply(Zlomek a) {
+        return zkratit(new Zlomek(citatel * a.citatel, jmenovatel * a.jmenovatel));
+    }
+
+    public Zlomek divide(Zlomek a) {
+        return zkratit(new Zlomek(citatel * a.jmenovatel, jmenovatel * a.citatel));
     }
 }
